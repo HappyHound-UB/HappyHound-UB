b8343fa7454cc21a9e3f67b83659c1285a0991dc
Malik Mati <malikmati90@gmail.com>
2023-04-22 19:46:08 +0200

    Commit Main

--- app/src/main/java/edu/ub/happyhound_app/MainActivity.java
+++ app/src/main/java/edu/ub/happyhound_app/MainActivity.java
@@ -1,6 +1,9 @@
 package edu.ub.happyhound_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,23 +15,22 @@ import android.widget.TextView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import edu.ub.happyhound_app.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
-    private TextView hello;
-    private Button btnLogOut;
     private FirebaseAuth mAuth;
     private FirebaseUser user;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         mAuth = FirebaseAuth.getInstance();
         user = mAuth.getCurrentUser();
-
-        btnLogOut = (Button) findViewById(R.id.btnLogOut);
-        hello = (TextView) findViewById(R.id.hello);
 
         if (user == null){
             Intent intent = new Intent(getApplicationContext(), LogIn.class);
@@ -36,17 +38,35 @@ public class MainActivity extends AppCompatActivity {
             finish();
         }
         else {
-            hello.setText(user.getEmail() + user.getDisplayName());
+            changeFragments(new HomeFragment());
         }
 
-        btnLogOut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mAuth.signOut();
-                Intent intent = new Intent(getApplicationContext(), LogIn.class);
-                startActivity(intent);
-                finish();
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()){
+                case R.id.home:
+                    changeFragments(new HomeFragment());
+                    break;
+                case R.id.add:
+                    changeFragments(new AddFragment());
+                    break;
+                case R.id.profile:
+                    changeFragments(new ProfileFragment());
+                    break;
             }
+
+            return true;
         });
+    }
+
+    private void changeFragments(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction
+                .replace(R.id.frameLayout, fragment.getClass(), null)
+                .setReorderingAllowed(true)
+                .addToBackStack("name")
+                .commit();
+        //fragmentTransaction.commit();
     }
 }
