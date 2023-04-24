548def4e584ae2f5e306e86d223dba57f606f517
Martin March <mmarchrovira@gmail.com>
2023-04-24 16:05:14 +0200

    Lo que he hecho hasta ahora

---
+++ app/src/main/java/edu/ub/happyhound_app/CrearRecordatorio.java
@@ -0,0 +1,14 @@
+package edu.ub.happyhound_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CrearRecordatorio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_recordatorio);
+    }
+}
