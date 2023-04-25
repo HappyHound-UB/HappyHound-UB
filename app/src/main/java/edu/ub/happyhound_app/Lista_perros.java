ccd0c99a6b174d9d6da2270b704ef609422b442b
meuyea <113419615+meuyea@users.noreply.github.com>
2023-04-25 12:02:00 +0200

    creada sctivity lista

---
+++ app/src/main/java/edu/ub/happyhound_app/Lista_perros.java
@@ -0,0 +1,14 @@
+package edu.ub.happyhound_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Lista_perros extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_perros);
+    }
+}
